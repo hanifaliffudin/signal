77785e741a5ba1b58b0bfbc38ccdfacf444079b8
Hanif Aliffudin <hanifaliffudin@gmail.com>
2021-06-28 15:38:26 +0700

    hubungi kami nik no hp check

--- assets/js/script.js
+++ assets/js/script.js
@@ -61,3 +61,38 @@ if (tabs == "infoDetail1") {
 } else {
   $("#opener").addClass("show active");
 }
+
+// hubungi kami nik dan no hp
+// Restricts input for the given textbox to the given inputFilter.
+function setInputFilter(textbox, inputFilter) {
+  [
+    "input",
+    "keydown",
+    "keyup",
+    "mousedown",
+    "mouseup",
+    "select",
+    "contextmenu",
+    "drop",
+  ].forEach(function (event) {
+    textbox.addEventListener(event, function () {
+      if (inputFilter(this.value)) {
+        this.oldValue = this.value;
+        this.oldSelectionStart = this.selectionStart;
+        this.oldSelectionEnd = this.selectionEnd;
+      } else if (this.hasOwnProperty("oldValue")) {
+        this.value = this.oldValue;
+        this.setSelectionRange(this.oldSelectionStart, this.oldSelectionEnd);
+      } else {
+        this.value = "";
+      }
+    });
+  });
+}
+
+setInputFilter(document.getElementById("nik"), function (value) {
+  return /^-?\d*$/.test(value);
+});
+setInputFilter(document.getElementById("phone"), function (value) {
+  return /^-?\d*$/.test(value);
+});
